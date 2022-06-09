e97ddb0e9b1074b6de06e4cd3edd1b2e009b0efe
Tomasz Kalicki <tomics32@gmail.com>
2022-06-09 16:09:38 +0200

    Added changing between tabs on the Login/Register Modal

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthModalComponent } from './auth-modal/auth-modal.component';
-
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
@@ -9,7 +9,8 @@ import { AuthModalComponent } from './auth-modal/auth-modal.component';
     AuthModalComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   exports: [
     AuthModalComponent
