713c108a617e9dc8e880e8466fb131120a42a469
Tomasz Kalicki <tomics32@gmail.com>
2022-07-11 14:09:14 +0200

    Now user can upload mp4 files which are stored in firebase storage
    -Added uploading files into storage
    -Added reactive forms handling into upload forms
    -Added alert component into upload component while file is uploaded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AboutComponent } from './about/about.component'
 import { VideoModule } from './video/video.module';
 import { ClipComponent } from './clip/clip.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { AngularFireStorageModule} from '@angular/fire/compat/storage'
  
 
 @NgModule({
@@ -33,7 +34,8 @@ import { NotFoundComponent } from './not-found/not-found.component';
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
     AngularFirestoreModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireStorageModule
     
   ],
   providers: [],
--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { v4 as uuid} from 'uuid'
 
 @Component({
   selector: 'app-upload',
@@ -10,7 +13,7 @@ export class UploadComponent implements OnInit {
   file: File | null = null;
   nextStep = false;
 
-  constructor() { }
+  constructor(private storage: AngularFireStorage) { }
 
   ngOnInit(): void {
   }
@@ -23,8 +26,41 @@ export class UploadComponent implements OnInit {
     if(!this.file || this.file.type !== 'video/mp4') {
       return 
     }
+    this.title.setValue(
+      this.file.name.replace(/\.[^/.]+$/, '')
+    )
     this.nextStep = true;
-    console.log(this.file)
+    
   }
 
+  
+
+  title = new FormControl('',[
+    Validators.required,
+    Validators.minLength(3)
+  ])
+
+
+
+  uploadForm = new FormGroup({
+    title: this.title
+  })
+
+  showAlert = false;
+  alertMsg = 'Please wait! Your file is being uploaded.';
+  alertColor = 'blue';
+  inSubmission = false;
+
+  uploadFile() {
+    this.showAlert = true;
+    this.alertColor = 'blue';
+    this.alertMsg = 'Please wait! Your file is being uploaded.';
+    this.inSubmission = true;
+
+    const clipFileName = uuid()
+    const clipPath = `clips/${clipFileName}.mp4`
+
+
+    this.storage.upload(clipPath, this.file)
+  }
 }
