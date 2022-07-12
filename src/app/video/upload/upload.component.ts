257892d1c48941ec30f36d2d318fd80cad2ff652
Tomasz Kalicki <tomics32@gmail.com>
2022-07-12 17:45:38 +0200

    Alert changes
    -Modified upload component to display alert with the percentage of the file uploading progress
    -Added success alert after successully uploading file
    -Fetching the user id, user name, title and name of the file from the uploaded file

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
-import { v4 as uuid} from 'uuid'
+import { v4 as uuid} from 'uuid';
+import { last, switchMap } from 'rxjs/operators';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import firebase from 'firebase/compat/app';
 
 @Component({
   selector: 'app-upload',
@@ -12,8 +15,14 @@ export class UploadComponent implements OnInit {
   isDragover = false;
   file: File | null = null;
   nextStep = false;
+  percentage = 0;
+  showPercentage = false;
+  user: firebase.User | null = null;
 
-  constructor(private storage: AngularFireStorage) { }
+  constructor(
+    private storage: AngularFireStorage,
+    private auth: AngularFireAuth
+    ) { auth.user.subscribe(user => this.user = user) }
 
   ngOnInit(): void {
   }
@@ -56,11 +65,46 @@ export class UploadComponent implements OnInit {
     this.alertColor = 'blue';
     this.alertMsg = 'Please wait! Your file is being uploaded.';
     this.inSubmission = true;
+    this.showPercentage = true;
 
-    const clipFileName = uuid()
-    const clipPath = `clips/${clipFileName}.mp4`
+    const clipFileName = uuid();
+    const clipPath = `clips/${clipFileName}.mp4`;
 
 
-    this.storage.upload(clipPath, this.file)
+    const task = this.storage.upload(clipPath, this.file);
+    const clipRef = this.storage.ref(clipPath);
+
+    task.percentageChanges().subscribe(progress =>{
+      this.percentage = progress as number / 100
+    });
+
+    task.snapshotChanges().pipe(
+      last(),
+      switchMap(() => clipRef.getDownloadURL())
+    ).subscribe({
+      next: (url) => {
+        const clip = {
+          uid: this.user?.uid,
+          displayName: this.user?.displayName,
+          title: this.title.value,
+          fileName: `${clipFileName}.mp4`,
+          url
+        }
+
+        console.log(clip);
+
+        this.alertColor = 'green';
+        this.alertMsg = 'Success! Your clip is now ready to share with the world.';
+        this.showPercentage = false;
+
+      },
+      error: (error) => {
+        this.alertColor = 'red';
+        this.alertMsg = 'Upload failed! Please try again later.';
+        this.inSubmission = true;
+        this.showPercentage = false;
+        console.error(error);
+      }
+    })
   }
 }
