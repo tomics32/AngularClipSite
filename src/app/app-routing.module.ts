4b922e2b783ae5efa3511719fb08a9fa6f88d888
Tomasz Kalicki <tomics32@gmail.com>
2022-08-04 11:09:01 +0200

    Completed Project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,6 +23,10 @@ const routes: Routes = [
     }
   },
   {
+    path:'', // dashboard/manage, dashboard/upload
+    loadChildren: async () => (await import('./video/video.module')).VideoModule
+  },
+  {
     path: '**',
     component: NotFoundComponent
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,6 @@ import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component'
-import { VideoModule } from './video/video.module';
 import { ClipComponent } from './clip/clip.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { AngularFireStorageModule} from '@angular/fire/compat/storage';
@@ -33,7 +32,6 @@ import { FbTimestampPipe } from './pipes/fb-timestamp.pipe'
   ],
   imports: [
     BrowserModule,
-    VideoModule,
     UserModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
